b07209a8aae5fe720334e9e69d3507b70cf19355
brunoczim <brunoczim@users.noreply.github.com>
2022-04-04 21:32:13 +0000

    deploy: 9a93f8e3b189b91b428fbac89f9b5a3dcc688711

---
+++ webio/callback/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Error","An error that might happen when the event completes."]],"struct":[["CallbackOnce","A handle to a oneshot callback registered in an event."],["Event","An event on which callbacks are registered."]],"type":[["AsyncCallback",""],["SyncCallback",""]]});
