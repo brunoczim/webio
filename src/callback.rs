8fe31c7fc3c433a56356c75bf7a3f846544a564e
brunoczim <brunoczim@gmail.com>
2022-04-04 21:18:29 -0300

    document callback

--- src/callback.rs
+++ src/callback.rs
@@ -1,28 +1,42 @@
-//! This module defines utilities for translating a callback into asyncrhonous
+//! This module defines utilities for translating a callback into asynchronous
 //! functions.
 
 use crate::panic::{CatchUnwind, Payload};
 use std::{cell::Cell, fmt, future::Future, panic, pin::Pin, rc::Rc, task};
 
-pub type SyncOnceCallback<'cb> = Box<dyn FnOnce() + 'cb>;
+/// The type of synchronous, oneshot callback handlers (i.e. the handler that
+/// calls callbacks): a boxed function.
+pub type SyncOnceCbHandler<'cb> = Box<dyn FnOnce() + 'cb>;
 
-pub type AsyncOnceCallback<'cb> = Pin<Box<dyn Future<Output = ()> + 'cb>>;
+/// The type of asynchronous, oneshot callback handlers (i.e. the handler that
+/// calls callbacks): a boxed future.
+pub type AsyncOnceCbHandler<'cb> = Pin<Box<dyn Future<Output = ()> + 'cb>>;
 
+/// Register of oneshot callbacks into an event, where the callback is
+/// syncrhonous (waiting for the callback to complete is still asynchronous).
+#[derive(Debug, Clone, Copy)]
 pub struct SyncOnceRegister<F> {
     register_fn: F,
 }
 
 impl<F> SyncOnceRegister<F> {
+    /// Creates a new register using an inner register function. Such function
+    /// receives callbacks handlers and register them. Callback handlers are
+    /// functions that are called when the event completes, and then, they call
+    /// the actual callbacks.
     pub fn new<'cb, T>(register_fn: F) -> Self
     where
-        F: FnOnce(SyncOnceCallback<'cb>) -> T,
+        F: FnOnce(SyncOnceCbHandler<'cb>) -> T,
     {
         Self { register_fn }
     }
 
+    /// Registers a callback and lets it listen for the target event. This
+    /// method is asyncrhonous: a future is returned, and when awaited, it waits
+    /// for the callback to complete.
     pub fn listen<'cb, C, U>(self, callback: C) -> OnceHandle<U>
     where
-        F: FnOnce(SyncOnceCallback<'cb>),
+        F: FnOnce(SyncOnceCbHandler<'cb>),
         C: FnOnce() -> U + 'cb,
         U: 'cb,
     {
@@ -30,12 +44,17 @@ impl<F> SyncOnceRegister<F> {
         callback_handle
     }
 
+    /// Registers a callback and lets it listen for the target event. This
+    /// method is asyncrhonous: a future is returned, and when awaited, it
+    /// waits for the callback to complete. The register can also return a
+    /// value, and so, this method returns both the register's return value
+    /// and the wait-future.
     pub fn listen_returning<'cb, C, T, U>(
         self,
         callback: C,
     ) -> (T, OnceHandle<U>)
     where
-        F: FnOnce(SyncOnceCallback<'cb>) -> T,
+        F: FnOnce(SyncOnceCbHandler<'cb>) -> T,
         C: FnOnce() -> U + 'cb,
         U: 'cb,
     {
@@ -51,39 +70,54 @@ impl<F> SyncOnceRegister<F> {
                 Err(payload) => callback_handle.panicked(payload),
             }
         });
-        let ret = (self.register_fn)(boxed_fn as SyncOnceCallback);
+        let ret = (self.register_fn)(boxed_fn as SyncOnceCbHandler);
 
         (ret, callback_once)
     }
 }
 
+/// Register of oneshot callbacks into an event, where the callback is
+/// asyncrhonous (waiting for the callback to complete is also asynchronous).
+#[derive(Debug, Clone, Copy)]
 pub struct AsyncOnceRegister<F> {
     register_fn: F,
 }
 
 impl<F> AsyncOnceRegister<F> {
+    /// Creates a new register using an inner register function. Such function
+    /// receives callbacks handlers and register them. Callback handlers are
+    /// futures that are awaited when the event completes, and then, they await
+    /// the actual callbacks.
     pub fn new<'cb, T>(register_fn: F) -> Self
     where
-        F: FnOnce(AsyncOnceCallback<'cb>) -> T,
+        F: FnOnce(AsyncOnceCbHandler<'cb>) -> T,
     {
         Self { register_fn }
     }
 
+    /// Registers a callback and lets it listen for the target event. This
+    /// method is asynchronous: a future is returned, and when awaited, it
+    /// waits for the callback to complete.
     pub fn listen<'cb, A>(self, callback: A) -> OnceHandle<A::Output>
     where
-        F: FnOnce(AsyncOnceCallback<'cb>),
+        F: FnOnce(AsyncOnceCbHandler<'cb>),
         A: Future + 'cb,
     {
         let (_, callback_once) = self.listen_returning(callback);
         callback_once
     }
 
+    /// Registers a callback and lets it listen for the target event. This
+    /// method is asynchronous: a future is returned, and when awaited, it
+    /// waits for the callback to complete. The register can also return a
+    /// value, and so, this method returns both the register's return value
+    /// and the wait-future.
     pub fn listen_returning<'cb, A, T>(
         self,
         callback: A,
     ) -> (T, OnceHandle<A::Output>)
     where
-        F: FnOnce(AsyncOnceCallback<'cb>) -> T,
+        F: FnOnce(AsyncOnceCbHandler<'cb>) -> T,
         A: Future + 'cb,
     {
         let shared = Rc::new(Shared::init_connected());
@@ -98,7 +132,7 @@ impl<F> AsyncOnceRegister<F> {
                 Err(payload) => callback_handle.panicked(payload),
             }
         });
-        let ret = (self.register_fn)(boxed_fut as AsyncOnceCallback);
+        let ret = (self.register_fn)(boxed_fut as AsyncOnceCbHandler);
 
         (ret, callback_once)
     }
