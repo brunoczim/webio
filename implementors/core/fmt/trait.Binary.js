716ac1ccc529611f1ec0ae324a799aa87d87421b
brunoczim <brunoczim@users.noreply.github.com>
2022-04-08 03:49:01 +0000

    deploy: 8d51f6611d3a5583c6753f2e322c0b4b69aad6ff

--- implementors/core/fmt/trait.Binary.js
+++ implementors/core/fmt/trait.Binary.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["js_sys"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.59.0/core/fmt/trait.Binary.html\" title=\"trait core::fmt::Binary\">Binary</a> for <a class=\"struct\" href=\"js_sys/struct.BigInt.html\" title=\"struct js_sys::BigInt\">BigInt</a>","synthetic":false,"types":["js_sys::BigInt"]}];
+implementors["js_sys"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/core/fmt/trait.Binary.html\" title=\"trait core::fmt::Binary\">Binary</a> for <a class=\"struct\" href=\"js_sys/struct.BigInt.html\" title=\"struct js_sys::BigInt\">BigInt</a>","synthetic":false,"types":["js_sys::BigInt"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
