439d6371332dc1c37f06c6ba23c851621f356085
brunoczim <brunoczim@gmail.com>
2022-05-10 21:44:02 -0300

    fix random safari error

--- test.sh
+++ test.sh
@@ -4,14 +4,15 @@ set -e
 
 cargo test --no-default-features
 cargo test --all-features
-wasm-pack test --node --no-default-features
+wasm-pack test --node -- --no-default-features
 wasm-pack test --node
-wasm-pack test --node --all-features
-wasm-pack test --firefox --headless --no-default-features
+wasm-pack test --node -- --all-features
+wasm-pack test --firefox --headless -- --no-default-features
 wasm-pack test --firefox --headless
-wasm-pack test --firefox --headless --all-features
-wasm-pack test --chrome --headless --no-default-features
+wasm-pack test --firefox --headless -- --all-features
+wasm-pack test --chrome --headless -- --no-default-features
 wasm-pack test --chrome --headless
-wasm-pack test --chrome --headless --all-features
-# wasm-pack test --safari --headless --no-default-features
-# wasm-pack test --safari --headless --all-features
+wasm-pack test --chrome --headless -- --all-features
+wasm-pack test --safari --headless -- --no-default-features
+# wasm-pack test --safari --headless
+# wasm-pack test --safari --headless -- --all-features
